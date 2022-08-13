c6cb5d5e42d3d973d6a34830498b81508c844161
Igor Anic <ianic@inet.hr>
2022-08-13 21:12:16 +0200

    add another option to flush board

--- blink/build.zig
+++ blink/build.zig
@@ -28,18 +28,19 @@ pub fn build(b: *std.build.Builder) !void {
     );
     b.getInstallStep().dependOn(&bin.step);
 
-    const bin_path = b.getInstallPath(.{ .bin = .{} }, exe.out_filename);
-    const flash_cmd = b.addSystemCommand(&[_][]const u8{
-        "/Applications/STMicroelectronics/STM32Cube/STM32CubeProgrammer/STM32CubeProgrammer.app/Contents/MacOs/bin/STM32_Programmer_CLI",
-        "-c",
-        "port=SWD",
-        "-d",
-        bin_path,
-        "--go",
-    });
+    // flash command using stm32 cube programmer cli
+    // https://www.st.com/en/development-tools/stm32cubeprog.html
+    const exe_path = b.getInstallPath(.{ .bin = .{} }, exe.out_filename);
+    const flash_cmd = b.addSystemCommand(&[_][]const u8{ "/Applications/STMicroelectronics/STM32Cube/STM32CubeProgrammer/STM32CubeProgrammer.app/Contents/MacOs/bin/STM32_Programmer_CLI", "-c", "port=SWD", "-d", exe_path, "--go" });
     flash_cmd.step.dependOn(b.getInstallStep());
-    const flash_step = b.step("flash", "Flash and run the app on your STM32F411");
+    const flash_step = b.step("flash", "Flash with stm32 programmer and run the app on your board");
     flash_step.dependOn(&flash_cmd.step);
+
+    // flash using st-flash from https://github.com/stlink-org/stlink
+    const st_flash_cmd = b.addSystemCommand(&[_][]const u8{ "st-flash", "write", "zig-out/bin/board.bin", "0x08000000" });
+    st_flash_cmd.step.dependOn(b.getInstallStep());
+    const st_flash_step = b.step("st-flash", "Flash and run the app on your board");
+    st_flash_step.dependOn(&st_flash_cmd.step);
 
     b.default_step.dependOn(&exe.step);
     exe.install();
